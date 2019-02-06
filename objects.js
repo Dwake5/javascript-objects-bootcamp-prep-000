5ae9e9839235b13f98914041ffe097440ddc14c6
Danny Wakeling <wakeling101@hotmail.com>
2019-02-06 09:11:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,7 @@ let playlist = {
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+}
